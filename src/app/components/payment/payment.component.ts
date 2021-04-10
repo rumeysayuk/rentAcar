604f9233804fdd39b7626fcfd8f86fcaefa47008
Emircan Dalman <emircandalman28@gmail.com>
2021-04-10 17:36:20 +0300

    1

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -6,8 +6,7 @@ import {Rental} from '../../models/rental';
 import {RentalService} from '../../services/rental.service';
 import {ToastrService} from 'ngx-toastr';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {AuthService} from "../../services/auth.service";
-import {LocalStorageService} from "../../services/local-storage.service";
+import {LocalStorageService} from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-payment',
@@ -23,18 +22,18 @@ export class PaymentComponent implements OnInit {
   masterCardIcon = faCcMastercard;
   rental: Rental;
   creditCardForm: FormGroup;
-  cardHolderName:string;
-  cardNumber:number;
-  expirationMonth:Date;
-  expirationYear:Date;
-  cvv:number;
+  cvv: string;
+  cardNumber: string;
+  expirationYear: number;
+  expirationMonth: number;
+  cardHolderName: string;
 
   constructor(private activatedRoute: ActivatedRoute,
               private rentalService: RentalService,
               private toastrService: ToastrService,
               private formBuilder: FormBuilder,
               private router: Router,
-              private localStorage:LocalStorageService) {
+              private localStorage: LocalStorageService) {
   }
 
   ngOnInit(): void {
@@ -44,43 +43,46 @@ export class PaymentComponent implements OnInit {
       }
     });
     this.createCreditCardForm();
-    this.getRental();
+    this.getSavedCard();
   }
 
   createCreditCardForm() {
     this.creditCardForm = this.formBuilder.group({
-      cardHolderName: ['', Validators.required],
-      cardNumber: ['', Validators.required],
-      expirationMonth: ['', Validators.required],
-      expirationYear: ['', Validators.required],
-      cvv: ['', Validators.required],
+      cardHolderName: [this.cardHolderName, Validators.required],
+      cardNumber: [this.cardNumber, Validators.required],
+      expirationMonth: [this.expirationMonth, Validators.required],
+      expirationYear: [this.expirationYear, Validators.required],
+      cvv: [this.cvv, Validators.required],
     });
   }
-  getRental(){
-var cardholdername= this.localStorage.getItem("cardholdername");
-var cardnumber=this.localStorage.getItem("cardnumber");
-var expirationMonth=this.localStorage.getItem("expirationmonth");
-var expirationYear=this.localStorage.getItem("expirationYear");
-var cvv=this.localStorage.getItem("cvv");
-console.log(cardnumber+ cvv)
 
+  getSavedCard() {
+    var cardHolderName = this.localStorage.getItem('cardholdername');
+    var cardNumber = this.localStorage.getItem('cardnumber');
+    var expirationMonth = this.localStorage.getItem('expirationmonth');
+    var expirationYear = this.localStorage.getItem('expirationYear');
+    var cvv = this.localStorage.getItem('cvv');
+    if (cardHolderName != null && cardNumber != null && expirationMonth != null && expirationYear != null && cvv != null) {
+      this.cardHolderName = cardHolderName;
+      this.cardNumber = cardNumber;
+      this.cvv = cvv;
+      this.expirationMonth = parseInt(expirationMonth);
+      this.expirationYear = parseInt(expirationYear);
+    }
   }
 
-  checked():boolean{
-    if()
-  }
   addRental() {
-      document.getElementById("saveCreditCard")
-      let cardModel = Object.assign({}, this.creditCardForm.value);
-      this.rentalService.addRental(this.rental, cardModel).subscribe(response => {
-      this.localStorage.setItem("cardholdername",cardModel.cardHolderName);
-      this.localStorage.setItem("cardnumber",cardModel.cardNumber);
-      this.localStorage.setItem("expirationmonth",cardModel.expirationMonth);
-      this.localStorage.setItem("expirationYear",cardModel.expirationYear);
-      this.localStorage.setItem("cvv",cardModel.cvv);
+    let cardModel = Object.assign({}, this.creditCardForm.value);
+    let saveCreditCardInput = <HTMLInputElement> document.getElementById('saveCreditCard');
+    if (saveCreditCardInput.checked == true) {
+      this.localStorage.setItem('cardholdername', cardModel.cardHolderName);
+      this.localStorage.setItem('cardnumber', cardModel.cardNumber);
+      this.localStorage.setItem('expirationmonth', cardModel.expirationMonth);
+      this.localStorage.setItem('expirationYear', cardModel.expirationYear);
+      this.localStorage.setItem('cvv', cardModel.cvv);
+    }
+    this.rentalService.addRental(this.rental, cardModel).subscribe(response => {
       this.toastrService.info(response.message);
-
-
       this.router.navigate(['/']).then(() => setTimeout(function() {
         window.location.reload();
       }, 800));
